c7056c27c43275d207aa35c6a41f4b3f371df975
sdquang75 <sdquang75@gmail.com>
2026-08-13 16:08:14 +0900

    Twofer

---
+++ java/two-fer/src/test/java/TwoferTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TwoferTest {
+
+    private Twofer twofer;
+
+    @BeforeEach
+    public void setup() {
+        twofer = new Twofer();
+    }
+
+    @Test
+    @DisplayName("no name given")
+    public void noNameGiven() {
+        assertThat(twofer.twofer(null))
+                .isEqualTo("One for you, one for me.");
+    }
+
+
+    @Test
+    @DisplayName("a name given")
+    public void aNameGiven() {
+        assertThat(twofer.twofer("Alice"))
+                .isEqualTo("One for Alice, one for me.");
+    }
+
+
+    @Test
+    @DisplayName("another name given")
+    public void anotherNameGiven() {
+        assertThat(twofer.twofer("Bob"))
+                .isEqualTo("One for Bob, one for me.");
+    }
+
+}
