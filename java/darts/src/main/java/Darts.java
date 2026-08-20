f0da18b23dba0b7b71da98a682ea7f3319b7a30e
sdquang75 <sdquang75@gmail.com>
2026-08-20 15:43:25 +0900

    Darts

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -1,5 +1,16 @@
 class Darts {
     int score(double xOfDart, double yOfDart) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+//        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        double khoangcach = xOfDart * xOfDart + yOfDart * yOfDart;
+        if (khoangcach <= 1) {
+            return 10;
+        }
+        if (khoangcach <= 25) {
+            return 5;
+        }
+        if (khoangcach <= 100) {
+            return 1;
+        }
+        return 0;
     }
 }
--- java/darts/src/test/java/DartsTest.java
+++ java/darts/src/test/java/DartsTest.java
@@ -13,63 +13,63 @@ public class DartsTest {
         assertThat(darts.score(-9, 9)).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("On the outer circle")
     public void onTheOuterCircle() {
         assertThat(darts.score(0, 10)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("On the middle circle")
     public void onTheMiddleCircle() {
         assertThat(darts.score(-5, 0)).isEqualTo(5);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("On the inner circle")
     public void onTheInnerCircle() {
         assertThat(darts.score(0, -1)).isEqualTo(10);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Exactly on center")
     public void exactlyOnCentre() {
         assertThat(darts.score(0, 0)).isEqualTo(10);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Near the center")
     public void nearTheCentre() {
         assertThat(darts.score(-0.1, -0.1)).isEqualTo(10);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just within the inner circle")
     public void justWithinTheInnerCircle() {
         assertThat(darts.score(0.7, 0.7)).isEqualTo(10);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just outside the inner circle")
     public void justOutsideTheInnerCircle() {
         assertThat(darts.score(0.8, -0.8)).isEqualTo(5);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just within the middle circle")
     public void justWithinTheMiddleCircle() {
         assertThat(darts.score(-3.5, 3.5)).isEqualTo(5);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just outside the middle circle")
     public void justOutsideTheMiddleCircle() {
@@ -77,21 +77,21 @@ public class DartsTest {
     }
 
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just within the outer circle")
     public void justWithinTheOuterCircle() {
         assertThat(darts.score(-7.0, 7.0)).isEqualTo(1);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Just outside the outer circle")
     public void justOutsideTheOuterCircle() {
         assertThat(darts.score(7.1, -7.1)).isEqualTo(0);
     }
 
-    @Disabled("Remove to run test")
+
     @Test
     @DisplayName("Asymmetric position between the inner and middle circles")
     public void asymmetricPositionBetweenTheInnerAndMiddleCircles() {
