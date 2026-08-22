daf87798d7042bf9a599303a48d9d2e2a6fc2433
sdquang75 <sdquang75@gmail.com>
2026-08-23 03:28:01 +0900

    Grains

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -3,11 +3,29 @@ import java.math.BigInteger;
 class Grains {
 
     BigInteger grainsOnSquare(final int square) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //    throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        BigInteger grains = BigInteger.ONE;
+        if (square < 1 || square > 64) {
+            throw new IllegalArgumentException("square must be between 1 and 64");
+        }
+        for (int i = 1; i < square; i++) {
+            grains = grains.multiply(BigInteger.TWO);
+
+        }
+
+        return grains;
+
+
     }
 
     BigInteger grainsOnBoard() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //    throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        BigInteger tong = BigInteger.ZERO;
+        for (int i = 1; i <= 64; i++) {
+            tong = tong.add(grainsOnSquare(i));
+        }
+        return tong;
     }
 
 }
--- java/grains/src/test/java/GrainsTest.java
+++ java/grains/src/test/java/GrainsTest.java
@@ -18,7 +18,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("1"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 2")
     public void countAtSquare2() {
@@ -26,7 +26,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("2"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 3")
     public void countAtSquare3() {
@@ -34,7 +34,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("4"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 4")
     public void countAtSquare4() {
@@ -42,7 +42,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("8"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 16")
     public void countAtSquare16() {
@@ -50,7 +50,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("32768"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 32")
     public void countAtSquare32() {
@@ -58,7 +58,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("2147483648"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("grains on square 64")
     public void countAtSquare64() {
@@ -66,7 +66,7 @@ public class GrainsTest {
         assertThat(result).isEqualTo(new BigInteger("9223372036854775808"));
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("square 0 is invalid")
     public void errorOnNullBoardSize() {
@@ -75,7 +75,7 @@ public class GrainsTest {
                 .withMessage("square must be between 1 and 64");
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("negative square is invalid")
     public void errorOnNegativeBoardSize() {
@@ -84,7 +84,7 @@ public class GrainsTest {
                 .withMessage("square must be between 1 and 64");
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("square greater than 64 is invalid")
     public void errorOnExcessiveBoardSize() {
@@ -93,7 +93,7 @@ public class GrainsTest {
                 .withMessage("square must be between 1 and 64");
     }
 
-    @Disabled("Remove to run test")
+    
     @Test
     @DisplayName("returns the total number of grains on the board")
     public void totalNumberOfGrainsOnABoard() {
